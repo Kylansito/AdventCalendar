d7df91124163308f79aad6cfaafe7c50f7913ba8
Kylansito <99259620+Kylansito@users.noreply.github.com>
2022-12-03 02:28:35 +0100

    P1 & P2 DONE

--- D1AdventCalendar/src/Main.java
+++ D1AdventCalendar/src/Main.java
@@ -4,11 +4,43 @@ public class Main {
     public static void main(String[] args) throws IOException {
         File f = new File("../AdventCalendar/D1Input.txt");
 
-        BufferedReader bur = new BufferedReader(new FileReader("../D1AdventCalendar/D1Input.txt"));
+        BufferedReader bur = new BufferedReader(new FileReader("./D1Input.txt"));
+        /*
+        ********
+        PART ONE
+        ********
+
+            String elf = bur.readLine();
+            long sumtot = 0;
+            long sumret = 0;
+            boolean acabar = false;
+
+            while(Integer.parseInt(elf) > 0) {
+                sumtot = 0;
+                while (!elf.equals("")) {
+                    sumtot += Integer.parseInt(elf);
+                    elf = bur.readLine();
+                }
+                if (sumtot > sumret) {
+                    sumret = sumtot;
+                }
+
+                elf = bur.readLine();
+
+            }
+            System.out.print(sumret);
+
+
+
+
+        ********
+        PART TWO
+        ********
+
         String elf = bur.readLine();
-        long sumtot = 0;
-        long sumret = 0;
-        boolean acabar = false;
+        int sumtot = 0;
+        int[] taulaRet = new int[3];
+        taulaRet[0] = 0;taulaRet[1] = 0;taulaRet[2] = 0;
 
         while(Integer.parseInt(elf) > 0) {
             sumtot = 0;
@@ -16,13 +48,27 @@ public class Main {
                 sumtot += Integer.parseInt(elf);
                 elf = bur.readLine();
             }
-            if (sumtot > sumret) {
-                sumret = sumtot;
-            }
+            taulaRet = taula(taulaRet, sumtot);
 
             elf = bur.readLine();
 
         }
-        System.out.print(sumret);
+        System.out.print(taulaRet[0]+taulaRet[1]+taulaRet[2]);
+        */
+
     }
+
+    private static int[] taula(int t[], int a){
+        if(a > t[0]) {
+            t[0] = a;
+        } else if(a > t[1]){
+                t[1] = a;
+            }
+        else if(a > t[2]){
+                t[2] = a;
+            }
+        return t;
+    }
+
+
 }

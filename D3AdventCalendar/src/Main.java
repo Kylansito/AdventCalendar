c3ab45cfce0df0195bdba5dfbe688b0e07508127
Kylansito <99259620+Kylansito@users.noreply.github.com>
2022-12-04 18:01:06 +0100

    Part 1 & Part 2 DONE

--- D3AdventCalendar/src/Main.java
+++ D3AdventCalendar/src/Main.java
@@ -1,9 +1,15 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.SortedSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader bur = new BufferedReader(new FileReader("./InputDay3.txt"));
+
+        /*
+
+        PART 1
 
         String in = bur.readLine();
         long sumret = 0;
@@ -20,84 +26,90 @@ public class Main {
         }
         System.out.print(sumret);
 
+
+
+        PART 2
+
+        String in = bur.readLine();
+        long sumret = 0;
+
+        while(in != null) {
+            int cont = 0;
+            char[] charT1 = null;
+            char[] charT2 = null;
+            char[] charT3 = null;
+            while (cont < 3) {
+                switch (cont) {
+                    case 0:
+                        charT1 = in.toCharArray();
+                        break;
+                    case 1:
+                        charT2 = in.toCharArray();
+                        break;
+                    case 2:
+                        charT3 = in.toCharArray();
+                        break;
+                }
+                in = bur.readLine();
+                cont++;
+            }
+
+            char matching = matchingCharArrayList(matchingChars(charT1, charT2), charT3);
+
+            sumret += valueOfChar(matching);
+
+        }
+        System.out.print(sumret);
+
+         */
+
+
+    }
+
+    private static ArrayList matchingChars(char[] c1, char[] c2){
+        ArrayList ret = new ArrayList<>();
+
+        for(int i = 0; i < c1.length; i++){
+            for (int j = 0; j < c2.length; j++) {
+                if (c1[i] == c2[j]) {
+                    ret.add(c2[j]);
+                }
+            }
+        }
+        return ret;
+
+    }
+
+    private static char matchingCharArrayList(ArrayList arr, char[] table){
+        for(int i = 0; i < table.length; i++){
+
+            if(arr.contains(table[i])){
+                return table[i];
+            }
+        }
+        return 'a';
     }
 
     private static char matchingChar(String str1, String str2){
         for(int i = 0; i < str1.length(); i++){
-            boolean contains = false;
 
             char[] charA = str1.toCharArray();
 
             for (char c : charA) {
                 if (c == str2.charAt(i)) {
-                    contains = true;
-                    break;
+                    return str2.charAt(i);
                 }
-            }
-            if (contains) {
-                return str2.charAt(i);
             }
         }
         return 'a';
     }
 
     private static int valueOfChar(char c){
-        switch(c){
-            case 'a': return 1;
-            case 'b':return 2;
-            case 'c':return 3;
-            case 'd':return 4;
-            case 'e':return 5;
-            case 'f':return 6;
-            case 'g':return 7;
-            case 'h':return 8;
-            case 'i':return 9;
-            case 'j':return 10;
-            case 'k':return 11;
-            case 'l':return 12;
-            case 'm':return 13;
-            case 'n':return 14;
-            case 'o':return 15;
-            case 'p':return 16;
-            case 'q':return 17;
-            case 'r':return 18;
-            case 's':return 19;
-            case 't':return 20;
-            case 'u':return 21;
-            case 'v':return 22;
-            case 'w':return 23;
-            case 'x':return 24;
-            case 'y':return 25;
-            case 'z':return 26;
-
-            case 'A': return 27;
-            case 'B':return 28;
-            case 'C':return 29;
-            case 'D':return 30;
-            case 'E':return 31;
-            case 'F':return 32;
-            case 'G':return 33;
-            case 'H':return 34;
-            case 'I':return 35;
-            case 'J':return 36;
-            case 'K':return 37;
-            case 'L':return 38;
-            case 'M':return 39;
-            case 'N':return 40;
-            case 'O':return 41;
-            case 'P':return 42;
-            case 'Q':return 43;
-            case 'R':return 44;
-            case 'S':return 45;
-            case 'T':return 46;
-            case 'U':return 47;
-            case 'V':return 48;
-            case 'W':return 49;
-            case 'X':return 50;
-            case 'Y':return 51;
-            case 'Z':return 52;
-
-            default: return -1;
+        if(c>='a'&&c<='z'){
+            return c-96;
         }
+        return c-38;
     }
+
+
 }

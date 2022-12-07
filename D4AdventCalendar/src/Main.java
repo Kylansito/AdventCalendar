231398b6a057d9f648dc09b521d7c753d22ad678
Kylansito <99259620+Kylansito@users.noreply.github.com>
2022-12-07 19:29:42 +0100

    P1 & P2 DONE

--- D4AdventCalendar/src/Main.java
+++ D4AdventCalendar/src/Main.java
@@ -3,6 +3,10 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
 
+        /*
+
+        PART 1
+
         BufferedReader bur = new BufferedReader(new FileReader("./D4Input.txt"));
 
         String in = bur.readLine();
@@ -43,6 +47,54 @@ public class Main {
             in = bur.readLine();
         }
         System.out.print(sumret);
+
+
+
+
+        PART 2
+
+        BufferedReader bur = new BufferedReader(new FileReader("./D4Input.txt"));
+
+        String in = bur.readLine();
+        long sumret = 0;
+
+        while (in != null) {
+            int min1 = 0;
+            int max1 = 0;
+
+            int min2 = 0;
+            int max2 = 0;
+            String num1 = "";
+            for (int i = 0; i < whereIsComma(in); i++) {
+
+                if (in.charAt(i) != '-') {
+                    num1 += in.charAt(i);
+                } else if (in.charAt(i) == '-') {
+                    min1 = Integer.parseInt(num1);
+                    max1 = Integer.parseInt(in.substring(i+1, whereIsComma(in)));
+                }
+
+            }
+            num1 = "";
+            for (int i = whereIsComma(in) +1; i < in.length(); i++) {
+                if (in.charAt(i) != '-') {
+                    num1 += in.charAt(i);
+                } else if (in.charAt(i) == '-') {
+                    min2 = Integer.parseInt(num1);
+                    max2 = Integer.parseInt(in.substring(i+1));
+                }
+
+            }
+
+            if(min1 <= min2 && max1 >= min2 || min2 <= min1 && max2 >= min1){
+                sumret++;
+            }
+
+            in = bur.readLine();
+        }
+        System.out.print(sumret);
+
+         */
 
     }
 
